4120ff370c9ff818b76a3b27eb765a0feaaae9df
Muhammad Taha <mtaha.dev@gmail.com>
2022-09-24 12:59:36 +0500

    upated payment table

--- panda-frontend/src/environments/environment.prod.ts
+++ panda-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url : "http://202.182.106.2/panda-backend/public/api/",
+  url : "http://45.32.51.81/backend/public/api/",
   recaptcha:{
 
     siteKey: '6LeobFcfAAAAAKq1WajqgRLme2MOjxzMHUEiM6Z7',
